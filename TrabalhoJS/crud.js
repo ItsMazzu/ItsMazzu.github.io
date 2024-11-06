f4ca362497e28bc4354d0bd5503a4fe60c62ee70
Mazzu <italo.itacom@gmail.com>
2024-11-05 21:47:50 -0300

    Update crud.js

--- TrabalhoJS/crud.js
+++ TrabalhoJS/crud.js
@@ -9,7 +9,6 @@ function fetchPokemon() {
             let peso = data.weight/10;
             let correcaoAltura;
             let correcaoPeso;
-            let shinySpriteUrl = data.sprites.front_default.replace('front_default', 'front_shiny');
             if (altura >= 100) {
                 correcaoAltura = (altura / 100).toFixed(2) + "m";
               } else {
@@ -24,7 +23,7 @@ function fetchPokemon() {
             pokemonInfo.innerHTML = `
                 <h2>${data.name}</h2>
                 <img src="${data.sprites.front_default}" alt="Sprite normal de ${data.name}">
-                <img src="${shinySpriteUrl}" alt="Sprite shiny de ${data.name}">
+                <img src="${data.sprites.front_shiny}" alt="Sprite shiny de ${data.name}">
                 <h3>Tipo(s)</h3>
                 <ul>
                     ${data.types.map(type => `<li>${type.type.name}</li>`).join('')}
